29208d0e952ecb1f3743aed4c4760ecc1d4bb32d
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2017-09-19 15:06:56 -0700

    linux-user: add SO_LINGER to getsockopt
    Signed-off-by: Carlo Marcelo Arenas Belón <carenas@gmail.com>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -3178,7 +3178,6 @@ static abi_long do_getsockopt(int sockfd, int level, int optname,
         level = SOL_SOCKET;
         switch (optname) {
         /* These don't just return a single integer */
-        case TARGET_SO_LINGER:
         case TARGET_SO_RCVTIMEO:
         case TARGET_SO_SNDTIMEO:
         case TARGET_SO_PEERNAME:
@@ -3211,6 +3210,39 @@ static abi_long do_getsockopt(int sockfd, int level, int optname,
             __put_user(cr.uid, &tcr->uid);
             __put_user(cr.gid, &tcr->gid);
             unlock_user_struct(tcr, optval_addr, 1);
+            if (put_user_u32(len, optlen)) {
+                return -TARGET_EFAULT;
+            }
+            break;
+        }
+        case TARGET_SO_LINGER:
+        {
+            struct linger lg;
+            socklen_t lglen;
+            struct target_linger *tlg;
+
+            if (get_user_u32(len, optlen)) {
+                return -TARGET_EFAULT;
+            }
+            if (len < 0) {
+                return -TARGET_EINVAL;
+            }
+
+            lglen = sizeof(lg);
+            ret = get_errno(getsockopt(sockfd, level, SO_LINGER,
+                                       &lg, &lglen));
+            if (ret < 0) {
+                return ret;
+            }
+            if (len > lglen) {
+                len = lglen;
+            }
+            if (!lock_user_struct(VERIFY_WRITE, tlg, optval_addr, 0)) {
+                return -TARGET_EFAULT;
+            }
+            __put_user(lg.l_onoff, &tlg->l_onoff);
+            __put_user(lg.l_linger, &tlg->l_linger);
+            unlock_user_struct(tlg, optval_addr, 1);
             if (put_user_u32(len, optlen)) {
                 return -TARGET_EFAULT;
             }
